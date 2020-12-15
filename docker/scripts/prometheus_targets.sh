c01ecfa403993d496a0ac3515764b841256c51a9
Rik Visser <rik.visser@philips.com>
2020-12-15 16:33:54 +0100

    Added cartel stuff from andriy

--- docker/scripts/prometheus_targets.sh
+++ docker/scripts/prometheus_targets.sh
@@ -7,5 +7,13 @@ echo $PROMETHEUS_TARGETS | base64 -d >> /sidecars/etc/configured_targets.yml
 # Merge the two yaml files together, overwriting prometheus.yml in place
 yq m --inplace --arrays append /sidecars/etc/prometheus.yml /sidecars/etc/configured_targets.yml
 
+# Things for Cartel
+json_tmpl_file="tmpl_targets"
+json_file="targets.json"
+echo $CARTEL_HOSTS | tr "," "\n" > hosts
+echo '[{"targets": '`cat hosts| jq -R -s -c 'split("\n")[:-1]'`', "labels":{"group":"cartel"}}]'  > $json_tmpl_file
+cat $json_tmpl_file | jq . > $json_file
+mv $json_file /sidecars/etc/
+
 # Start and run prometheus
 exec /sidecars/bin/prometheus "$@"
