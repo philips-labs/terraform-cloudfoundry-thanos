a7b633ec582147e382763ebbf77c6a9c5b442fc5
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-04-30 08:49:30 +0200

    Revert "Revert "Switch to upstream""
    This reverts commit 6a24de283934681112274d453d5f3afcbad990a6.

--- terraform.tf
+++ terraform.tf
@@ -3,8 +3,8 @@ terraform {
 
   required_providers {
     cloudfoundry = {
-      source  = "philips-labs/cloudfoundry"
-      version = ">= 0.1206.0"
+      source  = "cloudfoundry-community/cloudfoundry"
+      version = ">= 0.14.1"
     }
     random = {
       source  = "random"
