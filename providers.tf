e56a45ff758df02c6e4fb7efb7835b76251072cc
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-06-18 16:17:23 +0200

    Export Grafana details for use outside module

--- providers.tf
+++ providers.tf
@@ -1,7 +1,11 @@
 # NOTE: usage of provider config in the module makes it incompatible with for_each, count, and depends_on
 # Details available here: https://www.terraform.io/docs/language/modules/develop/providers.html
+locals {
+  grafana_auth = "${var.grafana_username}:${var.grafana_password}"
+  grafana_url  = "https://${local.grafana_endpoint}"
+}
 
 provider "grafana" {
-  url  = "https://${local.grafana_endpoint}"
-  auth = "${var.grafana_username}:${var.grafana_password}"
+  url  = local.grafana_url
+  auth = local.grafana_auth
 }
