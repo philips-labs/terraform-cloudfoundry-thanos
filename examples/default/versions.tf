5f764276d6e07821e87210c06c38216a889cb5a6
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-06-19 22:00:25 +0200

    Add missing provider entry

--- examples/default/versions.tf
+++ examples/default/versions.tf
@@ -3,6 +3,10 @@ terraform {
     cloudfoundry = {
       source = "cloudfoundry-community/cloudfoundry"
     }
+    hsdp = {
+      source  = "philips-software/hsdp"
+      version = ">= 0.17.0"
+    }
     grafana = {
       source  = "grafana/grafana"
       version = ">= 1.11.0"
