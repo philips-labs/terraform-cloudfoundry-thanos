174187f8f669ba2098ce264f3661d706941d8812
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-07-05 17:45:59 +0200

    Mark S3 service with thanos

--- services.tf
+++ services.tf
@@ -1,6 +1,6 @@
 
 resource "cloudfoundry_service_instance" "s3" {
-  name  = "tf-s3-${local.postfix}"
+  name  = "tf-s3-thanos-${local.postfix}"
   space = var.cf_space_id
   # TODO: This needs to be driven by the CF region
   service_plan = data.cloudfoundry_service.s3.service_plans["US Standard"]
