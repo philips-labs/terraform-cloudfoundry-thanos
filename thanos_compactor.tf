4d132ccf2d3a755741b635d6aa13577485c4390a
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-03-16 06:58:51 +0100

    Set correct health check

--- thanos_compactor.tf
+++ thanos_compactor.tf
@@ -1,9 +1,10 @@
 resource "cloudfoundry_app" "thanos_compactor" {
-  name         = "tf-thanos-compactor-${local.postfix}"
-  space        = var.cf_space_id
-  memory       = var.thanos_compactor_memory
-  disk_quota   = var.thanos_compactor_disk_quota
-  docker_image = var.thanos_compactor_image
+  name              = "tf-thanos-compactor-${local.postfix}"
+  space             = var.cf_space_id
+  memory            = var.thanos_compactor_memory
+  disk_quota        = var.thanos_compactor_disk_quota
+  docker_image      = var.thanos_compactor_image
+  health_check_type = "process"
   docker_credentials = {
     username = var.docker_username
     password = var.docker_password
